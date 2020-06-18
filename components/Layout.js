a8d8220ae3c09808a65d10b8e0a22d9d01bcc282
Marina <avalai@live.com>
2020-06-18 11:09:36 -0400

    Fix page height between toolbars

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,26 @@
 import BottomNav from '../components/BottomNav'
 import TopNav from '../components/TopNav'
+import { makeStyles } from '@material-ui/core/styles';
 import { Container } from '@material-ui/core'
 import { spacing } from '@material-ui/system';
 
+const useStyles = makeStyles((theme) => ({
+  toolbar: theme.mixins.toolbar,
+}))
+
 export default function Layout(props) {
+
+  const classes = useStyles()
+  
   return (
     <>
       <TopNav pageName={props.pageName} />
-      <Container style={{margin: "56px auto"}}>
+      <div className={classes.toolbar} />
+      <Container>
         {props.children}
       </Container>
+      {/* Bottom Toolbar spacer needs a top margin so the Fab btn doesn't overlap the content */}
+      <div className={classes.toolbar} style={{marginTop: '12px'}} />
       <BottomNav />
     </>
   )
